59d59e0babc9d7ebbed3c07ea90f9d8ae37cb4ad
Irlan Nonato <irlan.nonato97@hotmail.com>
2022-12-02 11:33:47 -0300

    Primeiro Commit

---
+++ funcao.js
@@ -0,0 +1,19 @@
+
+
+ Numb1 = () =>{
+    
+    let visor = document.querySelector('#visor-calc')
+    let numb1 = document.querySelector('#numb-1')    
+    let arr = ''
+
+    arr = '1'
+    visor.innerHTML += parseInt(arr) 
+    
+    
+    console.log(visor)
+    
+
+    
+
+
+}
