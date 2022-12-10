7deda968c68f16f6afc0c007be8b5edb19bcc66b
Irlan Nonato <irlan.nonato97@hotmail.com>
2022-12-10 14:10:26 -0300

    adicionando numeros a calc type int

--- funcao.js
+++ funcao.js
@@ -1,27 +1,53 @@
 
  
  
+ let visor = document.querySelector('#visor-calc')
+//  let discador = document.querySelector('.numb-button')
  
  
- 
- Calculadora = () =>{
-    
-    let visor = document.querySelector('#visor-calc')
-    let discador = document.querySelector('.numb-button')
-    
-    
-    switch (discador.value){
-        case '1':
-            visor.innerHTML += 1
-            
-            break
-        case '2':
-            visor.innerHTML += 2
-            
-            break;
-    }
-    
+ Calc_numb1 = () =>{
 
+    let discador = document.querySelector('#numb_1')
+    visor.innerHTML += parseInt(discador.value)
 }
+    
+ Calc_numb2 = () =>{
+    let discador = document.querySelector('#numb_2')
+    visor.innerHTML += parseInt(discador.value) 
+    
+ }
+ Calc_numb3 = () =>{
+    let discador = document.querySelector('#numb_3')
+    visor.innerHTML += parseInt(discador.value)
+ }
+ Calc_numb4 = () =>{
+    let discador = document.querySelector('#numb_4')
+    visor.innerHTML += parseInt(discador.value)
+ }
+ Calc_numb5 = () =>{
+    let discador = document.querySelector('#numb_5')
+    visor.innerHTML += parseInt(discador.value)
+ }
+ Calc_numb6 = () =>{
+    let discador = document.querySelector('#numb_6')
+    visor.innerHTML += parseInt(discador.value)
+ }
+ Calc_numb7 = () =>{
+    let discador = document.querySelector('#numb_7')
+    visor.innerHTML += parseInt(discador.value)
+ }
+ Calc_numb8 = () =>{
+    let discador = document.querySelector('#numb_8')
+    visor.innerHTML += parseInt(discador.value)
+ }
+ Calc_numb9 = () =>{
+    let discador = document.querySelector('#numb_9')
+    visor.innerHTML += parseInt(discador.value)
+ }
+ Calc_numb0 = () =>{
+    let discador = document.querySelector('#numb_0')
+    visor.innerHTML += parseInt(discador.value)
+ }
+
 
 
